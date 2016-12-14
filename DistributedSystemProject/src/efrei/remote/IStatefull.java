139b02b101c328e694a688af0f7d2469cc1ab920
franck-cussac <franck.cussac@gmail.com>
2016-12-14 09:25:30 +0100

    ajout des tags stateless et statefull

---
+++ DistributedSystemProject/src/efrei/remote/IStatefull.java
@@ -0,0 +1,9 @@
+package efrei.remote;
+
+import java.rmi.Remote;
+
+// juste un tag pour marquer un Remote comme étant Statefull
+
+public interface IStatefull extends Remote {
+
+}
---
+++ DistributedSystemProject/src/efrei/remote/IStateless.java
@@ -0,0 +1,9 @@
+package efrei.remote;
+
+import java.rmi.Remote;
+
+// juste un tag pour marque run Remote comme Stateless
+
+public interface IStateless extends Remote {
+
+}
